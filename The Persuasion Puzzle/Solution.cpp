73f419629507ca4aaf760485533681dc1686f16f
AlgoConnectSASTRA <161212994+AlgoConnectSASTRA@users.noreply.github.com>
2024-03-30 21:49:26 +0530

    Update Solution.cpp

--- The Persuasion Puzzle/Solution.cpp
+++ The Persuasion Puzzle/Solution.cpp
@@ -1,12 +1,12 @@
-int solution( long n, vector<long> m)
+int solution( long no_of_days, vector<long> tshirts_sold)
 {
-    int sum=0, ans=0;
-    for( long i=0; i<n; i++)
+    int sum=0, result=0;
+    for( long i=0; i<no_of_days; i++)
     {
-        sum+=m[i];
+        sum+=tshirts_sold[i];
         if( sum==i+1 )
-            ans++;
+            result++;
     }
 
-    return ans;
+    return result;
 }
