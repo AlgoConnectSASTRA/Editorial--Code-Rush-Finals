f01256755c7c3d37aec918f35d7cd45ff45468d9
AlgoConnectSASTRA <161212994+AlgoConnectSASTRA@users.noreply.github.com>
2024-03-30 21:47:44 +0530

    Update Solution.cpp

--- The Persuasion Puzzle/Solution.cpp
+++ The Persuasion Puzzle/Solution.cpp
@@ -1,6 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 int solution( long n, vector<long> m)
 {
     int sum=0, ans=0;
@@ -12,26 +9,4 @@ int solution( long n, vector<long> m)
     }
 
     return ans;
-}
-
-int main()
-{
-    long t;
-    cin >> t;
-
-    while(t--)
-    {
-        long n;
-        cin >> n;
-
-        vector<long> m(n);
-
-        for( long &i: m)
-            cin >> i;
-
-        cout << solution( n, m) << endl;
-
-    }
-
-    return 0;
 }
