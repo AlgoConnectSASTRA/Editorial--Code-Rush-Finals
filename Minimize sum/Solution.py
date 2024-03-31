d8f50077bad373671a1ad4c593b258819c44131a
AlgoConnectSASTRA <161212994+AlgoConnectSASTRA@users.noreply.github.com>
2024-03-31 16:51:00 +0530

    Update Solution.py

--- Minimize sum/Solution.py
+++ Minimize sum/Solution.py
@@ -1,18 +1,4 @@
 from math import gcd
-def solution(n,A):
-    all_even = all(num % 2 == 0 for num in A)
-    all_odd = all(num % 2 != 0 for num in A)
-    
-    if all_even or all_odd:
-        min_num = min(A)
-        max_num = max(A)
-        min_gcd = gcd(min_num, max_num)
-        return min_gcd * len(A)
-    else:
-        return len(A)
-
-#nlogn approach
-from math import gcd
 def solution2(n,A):
   ans=A[0]
   for i in A:
